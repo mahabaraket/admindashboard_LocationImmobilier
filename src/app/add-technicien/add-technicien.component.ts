ef37016d92859eaff426b26d24e5b29e41e78b57
aroussiteyeb <aroussi1996@gmail.com>
2021-11-23 12:00:58 +0100

    last change

--- src/app/add-technicien/add-technicien.component.ts
+++ src/app/add-technicien/add-technicien.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MustMatch } from '../_helpers/must-match.validator';
+import { TechService } from '../services/tech.service'; 
+
 
 
 
@@ -13,14 +15,13 @@ export class AddTechnicienComponent implements OnInit {
   addtechnicienForm: FormGroup;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,private crudService:TechService) { }
 
   ngOnInit(): void {
   this.addtechnicienForm = this.formBuilder.group({
     Prenom: ['', Validators.required],
     Nom: ['', Validators.required],
-    age:['', Validators.required],
-    specilalite:['', Validators.required],
+    specialite:['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     confirmPassword: ['', Validators.required]
@@ -38,9 +39,20 @@ export class AddTechnicienComponent implements OnInit {
       if (this.addtechnicienForm.invalid) {
           return;
       }
+   let Tech={
+    specialite:this.addtechnicienForm.controls.specialite.value,
+    email:this.addtechnicienForm.controls.email.value,
+    firstName:this.addtechnicienForm.controls.Nom.value,
+    lastName:this.addtechnicienForm.controls.Prenom.value,
+    password:this.addtechnicienForm.controls.password.value,
+    role:"tech"
+   }
+      this.crudService.createTech(Tech).subscribe((res)=>{
+        console.log("added succ",res);
+      });
 
       // display form values on success
-      alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.addtechnicienForm.value, null, 4));
+      //alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.addtechnicienForm.value, null, 4));
   }
   onReset() {
     this.submitted = false;
---
+++ src/app/services/tech.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+class User{
+
+  specialite:any;
+  email:any;
+  firstName:any;
+  lastName:any;
+  password:any;
+  role:any;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TechService {
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  private UsersUrl = 'http://localhost:8181/api/users';
+
+  constructor(private http:HttpClient) { }
+
+
+  getTech (): Observable<User[]> {
+    return this.http.get<User[]>(this.UsersUrl).pipe(
+      tap(_ => console.log('fetched Users')),
+      catchError(this.handleError<User[]>('getUsers', []))
+    );
+  }
+
+  createTech(User: User): Observable<any> {
+    return this.http.post<User>(this.UsersUrl, User, httpOptions).pipe(
+      tap((newUser: User) => console.log(`added User`)),
+      catchError(this.handleError<User>('create'))
+    );
+  }
+
+
+  updateTech(User: User,id:any): Observable<any> {
+    return this.http.put<User>(this.UsersUrl+"/"+ id,User, httpOptions).pipe(
+      tap((newUser: User) => console.log(`update User`)),
+      catchError(this.handleError<User>('update'))
+    );
+  }
+
+
+  deleteTech(id:any) {
+    return this.http.delete(this.UsersUrl+"/"+id).pipe(
+     // tap((newBien: bienImmobilier) => console.log(`update bien`)),
+      catchError(this.handleError<User>('delete'))
+    );
+  }
+
+  findTechByid(id:any) {
+    return this.http.get(this.UsersUrl+"/"+id).pipe(
+     // tap((newBien: bienImmobilier) => console.log(`update bien`)),
+      catchError(this.handleError<User>('delete'))
+    );
+  }
+}
